dfe3f3144be46bffe0b411123d02cb9c22184d38
nolan <nolanaguirre08@gmail.com>
2017-12-22 20:07:25 -0600

    handle post request for card data

--- api.js
+++ api.js
@@ -2,17 +2,19 @@ const express = require('express');
 const app = express();
 require('dotenv').config()
 const jwt = require('express-jwt');
+const bodyParser = require('body-parser');
 const jwtAuthz = require('express-jwt-authz');
 const jwksRsa = require('jwks-rsa');
 const cors = require('cors');
 const db = require('./db')
-
-
+// var fs = require('fs');
+// db.updateSets(JSON.parse(fs.readFileSync('AllSetsFormatted.json', 'utf8')));
 if (!process.env.AUTH0_DOMAIN || !process.env.AUTH0_AUDIENCE) {
   throw 'Make sure you have AUTH0_DOMAIN, and AUTH0_AUDIENCE in your .env file'
 }
 
 app.use(cors());
+app.use(bodyParser.json());
 
 const checkJwt = jwt({
   // Dynamically provide a signing key based on the kid in the header and the singing keys provided by the JWKS endpoint.
@@ -34,6 +36,19 @@ const checkScopesAdmin = jwtAuthz([ 'write:messages' ]);
 
 app.get('/api/public', function(req, res) {
   res.json({ message: "Hello from a public endpoint! You don't need to be authenticated to see this." });
+});
+
+app.post('/api/search', function(req, res) {
+  db.getCards(req.body.queryParams, function(data){
+    if(data){
+      data.forEach(function(card){
+        card.url = "https://magic-price-api.herokuapp.com/" + card.set.replace(' Edition', '').replace(/ /g, '+') + '/' + card.name.replace(/ /g, '+');
+      })
+      res.json(data)
+    }else{
+      res.json(null);
+    }
+  })
 });
 
 app.get('/api/private', checkJwt, checkScopes, function(req, res) {
