80b857b7b39c147a8ba62bcdb495e805a0eb9ac8
nolan <nolanaguirre08@gmail.com>
2017-12-22 20:07:48 -0600

    change one to many, handle error in callback

--- db.js
+++ db.js
@@ -1,15 +1,17 @@
 var express = require('express');
 var pgp = require('pg-promise')( /*options*/ )
 pgp.pg.defaults.ssl = true;
-var db ={};
+var db = {};
 var database = pgp(process.env.DATABASE_URL);
 
 db.getCards = function(queryParams, callback) {
-  database.many('SELECT FROM magic_cards WHERE name = $1)', queryParams.name)
-  .then(callback(data))
-  .catch(function(err) {
-    console.log(err);
-  });
+  database.many('SELECT * FROM magic_cards WHERE name = $1', queryParams.name)
+    .then(function(data) {
+      callback(data);
+    })
+    .catch(function(err) {
+      callback(null);
+    });
 }
 
 db.updateSets = function(json) {
@@ -18,11 +20,11 @@ db.updateSets = function(json) {
       database.one('SELECT EXISTS (SELECT 1 FROM magic_cards WHERE name = $1 AND set = $2)', [card.name, card.set])
         .then(function(data) {
           if (!data.exists) {
-            database.none('INSERT INTO magic_cards (multiverseid, name, variations, set) VALUES ($1, $2, $3, $4)', [card.multiverseid, card.name, card.variations, card.set])
-            .then(function(){})
-            .catch(function(err){
-              console.log(err);
-            });
+            database.none('INSERT INTO magic_cards (multiverseid, name, set, variations) VALUES ($1, $2, $3, $4)', [card.multiverseid, card.name, card.set,card.variations])
+              .then(function() {})
+              .catch(function(err) {
+                console.log(err);
+              });
           }
         })
         .catch(function(err) {
