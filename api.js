b75a88faed6e2660d92ad02979ab483b1354392f
nolan <nolanaguirre08@gmail.com>
2018-02-19 19:10:25 -0600

    create typeahead endpoint

--- api.js
+++ api.js
@@ -7,56 +7,82 @@ const jwtAuthz = require('express-jwt-authz');
 const jwksRsa = require('jwks-rsa');
 const cors = require('cors');
 const db = require('./db')
-// var fs = require('fs');
+
 // db.updateSets(JSON.parse(fs.readFileSync('AllSetsFormatted.json', 'utf8')));
 if (!process.env.AUTH0_DOMAIN || !process.env.AUTH0_AUDIENCE) {
-  throw 'Make sure you have AUTH0_DOMAIN, and AUTH0_AUDIENCE in your .env file'
+    throw 'Make sure you have AUTH0_DOMAIN, and AUTH0_AUDIENCE in your .env file'
 }
 
 app.use(cors());
 app.use(bodyParser.json());
 
 const checkJwt = jwt({
-  // Dynamically provide a signing key based on the kid in the header and the singing keys provided by the JWKS endpoint.
-  secret: jwksRsa.expressJwtSecret({
-    cache: true,
-    rateLimit: true,
-    jwksRequestsPerMinute: 5,
-    jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`
-  }),
+    // Dynamically provide a signing key based on the kid in the header and the singing keys provided by the JWKS endpoint.
+    secret: jwksRsa.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`
+    }),
 
-  // Validate the audience and the issuer.
-  audience: process.env.AUTH0_AUDIENCE,
-  issuer: `https://${process.env.AUTH0_DOMAIN}/`,
-  algorithms: ['RS256']
+    // Validate the audience and the issuer.
+    audience: process.env.AUTH0_AUDIENCE,
+    issuer: `https://${process.env.AUTH0_DOMAIN}/`,
+    algorithms: ['RS256']
 });
 
-const checkScopes = jwtAuthz([ 'read:messages' ]);
-const checkScopesAdmin = jwtAuthz([ 'write:messages' ]);
+const checkScopes = jwtAuthz(['read:messages']);
+const checkScopesAdmin = jwtAuthz(['write:messages']);
 
 app.get('/api/public', function(req, res) {
-  res.json({ message: "Hello from a public endpoint! You don't need to be authenticated to see this." });
+    res.json({
+        message: "Hello from a public endpoint! You don't need to be authenticated to see this."
+    });
+});
+
+app.post('/api/typeahead', function(req, res) {
+    console.log(req.body)
+    db.typeahead(req.body.queryParams, function(data){
+        if(data){
+            res.json(data.slice(0,10));
+        }
+    })
+});
+
+app.get('/api/typeahead', function(req, res) {
+    res.json({
+        message: "Hello from a public endpoint! You don't need to be authenticated to see this."
+    });
 });
 
 app.post('/api/search', function(req, res) {
-  db.getCards(req.body.queryParams, function(data){
-    if(data){
-      data.forEach(function(card){
-        card.url = "https://magic-price-api.herokuapp.com/" + card.set.replace(' Edition', '').replace(/ /g, '+') + '/' + card.name.replace(/ /g, '+');
-      })
-      res.json(data)
-    }else{
-      res.json(null);
+    function formatString(set, name){
+        return set.replace(/[^a-zA-Z\d\s]/g, "").replace(/ /g, '+') + '/' + name.replace(/[^a-zA-Z\d\s]/g, "").replace(/ /g, '+');
     }
-  })
+    console.log(req.body.queryParams);
+    db.getCards(req.body.queryParams, function(data) {
+        if (data) {
+            data.forEach(function(card) {
+                //  https://magic-price-api.herokuapp.com/
+                card.url = "http://localhost:3002/" + formatString(card.set, card.name);
+            })
+            res.json(data)
+        } else {
+            res.json(null);
+        }
+    })
 });
 
 app.get('/api/private', checkJwt, checkScopes, function(req, res) {
-  res.json({ message: "Hello from a private endpoint! You need to be authenticated and have a scope of read:messages to see this." });
+    res.json({
+        message: "Hello from a private endpoint! You need to be authenticated and have a scope of read:messages to see this."
+    });
 });
 
 app.post('/api/admin', checkJwt, checkScopesAdmin, function(req, res) {
-  res.json({ message: "Hello from an admin endpoint! You need to be authenticated and have a scope of write:messages to see this." });
+    res.json({
+        message: "Hello from an admin endpoint! You need to be authenticated and have a scope of write:messages to see this."
+    });
 });
 
 app.listen(3001);
--- db.js
+++ db.js
@@ -2,6 +2,7 @@ var express = require('express');
 var pgp = require('pg-promise')( /*options*/ )
 pgp.pg.defaults.ssl = true;
 var db = {};
+const fs = require('fs');
 var database = pgp(process.env.DATABASE_URL);
 
 db.getCards = function(queryParams, callback) {
@@ -13,6 +14,19 @@ db.getCards = function(queryParams, callback) {
       callback(null);
     });
 }
+
+db.typeahead = function(queryParams, callback){
+    database.many("SELECT DISTINCT name FROM magic_cards WHERE name LIKE $1", queryParams.name + '%')
+    .then(function(data){;
+        callback(data);
+    })
+    .catch(function(err){
+        callback(null);
+    })
+}
+
+
+
 
 db.updateSets = function(json) {
   for (var set in json) {
@@ -33,5 +47,25 @@ db.updateSets = function(json) {
     })
   }
 }
-
+db.getAllSets = function() {
+  database.many('SELECT * FROM magic_cards')
+    .then(function(data) {
+        let sets = []
+        let outputSets = [];
+        data.forEach(function(card){
+            if(!sets.includes(card.set)){
+                sets.push(card.set);
+                outputSets.push(card.set + " Name: " + card.name);
+            }
+        })
+        fs.writeFile('allSetNames.json', JSON.stringify(outputSets), 'utf8', function(err){
+          if(err){
+            console.log(err);
+          }
+        });
+    })
+    .catch(function(err) {
+      console.log(null);
+    });
+}
 module.exports = db;
