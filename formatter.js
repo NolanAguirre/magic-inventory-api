c7cbfcb1c074048bd7f6faa828b3209e47499540
nolan <nolanaguirre08@gmail.com>
2018-02-28 15:39:52 -0600

    update formatter to include more data

--- formatter.js
+++ formatter.js
@@ -1,5 +1,5 @@
 var fs = require('fs');
-var sets = JSON.parse(fs.readFileSync('AllSets.json', 'utf8'));
+var sets = JSON.parse(fs.readFileSync('card_data/AllSets.json', 'utf8'));
 for(var set in sets){
   delete sets[set].releaseDate;
   delete sets[set].border;
@@ -8,6 +8,7 @@ for(var set in sets){
   delete sets[set].translations;
   sets[set].cards.forEach(function(card){
     card.set = sets[set].name;
+    card.setCode = sets[set].code;
     delete card.artist;
     delete card.layout;
     delete card.watermark;
@@ -27,14 +28,14 @@ for(var set in sets){
     delete card.colors;
 
     delete card.id;
-    delete card.number;
+    //delete card.number;
     delete card.mciNumber;
     delete card.imageName;
     delete card.releaseDate;
     delete card.reserved;
   })
 }
-fs.writeFile('AllSetsFormatted.json', JSON.stringify(sets), 'utf8', function(err){
+fs.writeFile('card_data/AllSetsFormatted.json', JSON.stringify(sets), 'utf8', function(err){
   if(err){
     console.log(err);
   }
