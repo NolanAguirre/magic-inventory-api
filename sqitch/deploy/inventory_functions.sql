b0714ae7089f02e60b6494808d8d24ccfbabff07
Nolan Aguirre <nolanaguirre08@gmail.com>
2018-05-08 00:29:37 -0500

    update inventory functions to a working runnable state

--- sqitch/deploy/inventory_functions.sql
+++ sqitch/deploy/inventory_functions.sql
@@ -4,17 +4,29 @@
 BEGIN;
 
 CREATE FUNCTION magic_inventory.add_inventory(json[], TEXT) RETURNS VOID AS $$ -- json of store cards, and
+  DECLARE
+  temp_card json;
   BEGIN
-    FOR x IN SELECT * FROM json_array_elements($1)
+    FOR temp_card IN SELECT * FROM json_array_elements($1)
     LOOP
-      IF(SELECT EXISTS(SELECT 1 FROM magic_inventory.inventory WHERE store_id = $2 AND card.name = x.name AND card.set_name = x.name AND card.condition = x.condition)) THEN
-        UPDATE magic_inventory.inventory SET quantity = quantity + x.quantity WHERE store_id = $2 AND card.name = x.name AND card.set_name = x.name AND card.condition = x.condition)
+      IF(SELECT EXISTS(SELECT 1 FROM magic_inventory.inventory WHERE store_id = $2 AND card.name = temp_card.name AND card.set_name = temp_card.name AND card.condition = temp_card.condition)) THEN
+        UPDATE magic_inventory.inventory SET quantity = quantity + temp_card.quantity WHERE store_id = $2 AND card.name = temp_card.name AND card.set_name = temp_card.name AND card.condition = temp_card.condition;
       ELSE
-        INSERT INTO magic_inventory.inventory VALUES (magic_inventory.create_card(x), $2);
+        INSERT INTO magic_inventory.inventory VALUES (magic_inventory.create_card(i), $2);
       END IF;
     END LOOP;
   END
+$$ LANGUAGE PLPGSQL;
 
+CREATE FUNCTION magic_inventory.remove_inventory(json[], TEXT) RETURNS VOID AS $$
+  DECLARE
+  temp_card json;
+  BEGIN
+    FOR temp_card in SELECT * FROM json_array_elements($1)
+    LOOP
+
+    END LOOP;
+  END
 $$ LANGUAGE PLPGSQL;
 
 
