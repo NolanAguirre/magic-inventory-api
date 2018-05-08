df1ac625ee27199f2b30b51019b5f49e7bb4599f
Nolan Aguirre <nolanaguirre08@gmail.com>
2018-05-08 00:30:18 -0500

    update grants, GRANT ROLE -> GRANT

--- sqitch/deploy/roles.sql
+++ sqitch/deploy/roles.sql
@@ -6,18 +6,18 @@ CREATE ROLE magic_inventory_roles LOGIN PASSWORD 'potato'; --temporary password
 
 CREATE ROLE magic_inventory_anonymous;
 
-GRANT ROLE magic_inventory_anonymous TO magic_inventory_roles;
+GRANT magic_inventory_anonymous TO magic_inventory_roles;
 
 CREATE ROLE magic_inventory_user;
 
-GRANT ROLE magic_inventory_user TO magic_inventory_roles;
+GRANT magic_inventory_user TO magic_inventory_roles;
 
 CREATE ROLE magic_inventory_store_owner;
 
-GRANT ROLE magic_inventory_store_owner TO magic_inventory_roles;
+GRANT magic_inventory_store_owner TO magic_inventory_roles;
 
 CREATE ROLE magic_inventory_employee;
 
-GRANT ROLE magic_inventory_employee TO magic_inventory_roles;
+GRANT magic_inventory_employee TO magic_inventory_roles;
 
 COMMIT;
