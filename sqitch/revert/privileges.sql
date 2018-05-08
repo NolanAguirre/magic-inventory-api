635550f3da9cb2d492b5efe6f6b8b9c80923e807
Nolan Aguirre <nolanaguirre08@gmail.com>
2018-05-08 01:03:15 -0500

    add privileges revert

--- sqitch/revert/privileges.sql
+++ sqitch/revert/privileges.sql
@@ -2,6 +2,34 @@
 
 BEGIN;
 
--- XXX Add DDLs here.
+-- REVOKE ALL PRIVILEGES ON ALL TABLES IN SCHEMA public FROM magic_inventory_user;
+-- REVOKE ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public FROM magic_inventory_user;
+-- REVOKE ALL PRIVILEGES ON ALL FUNCTIONS IN SCHEMA public FROM magic_inventory_user;
+--
+-- REVOKE ALL PRIVILEGES ON ALL TABLES IN SCHEMA public FROM magic_inventory_anonymous;
+-- REVOKE ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public FROM magic_inventory_anonymous;
+-- REVOKE ALL PRIVILEGES ON ALL FUNCTIONS IN SCHEMA public FROM magic_inventory_anonymous;
+--
+-- REVOKE ALL PRIVILEGES ON ALL TABLES IN SCHEMA public FROM magic_inventory_employee;
+-- REVOKE ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public FROM magic_inventory_employee;
+-- REVOKE ALL PRIVILEGES ON ALL FUNCTIONS IN SCHEMA public FROM magic_inventory_employee;
+--
+-- REVOKE ALL PRIVILEGES ON ALL TABLES IN SCHEMA public FROM magic_inventory_store_owner;
+-- REVOKE ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public FROM magic_inventory_store_owner;
+-- REVOKE ALL PRIVILEGES ON ALL FUNCTIONS IN SCHEMA public FROM magic_inventory_store_owner;
+--
+-- REVOKE ALL PRIVILEGES ON ALL TABLES IN SCHEMA public FROM magic_inventory_roles;
+-- REVOKE ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public FROM magic_inventory_roles;
+-- REVOKE ALL PRIVILEGES ON ALL FUNCTIONS IN SCHEMA public FROM magic_inventory_roles;
+
+DROP OWNED BY magic_inventory_store_owner;
+
+DROP OWNED BY magic_inventory_user;
+
+DROP OWNED BY magic_inventory_employee;
+
+DROP OWNED BY magic_inventory_anonymous;
+
+DROP OWNED BY magic_inventory_roles;
 
 COMMIT;
