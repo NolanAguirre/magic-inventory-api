a308a6aa4194d3e463f78ad873e0eb428cdcfcf4
Nolan Aguirre <nolanaguirre08@gmail.com>
2018-05-08 00:30:40 -0500

    add create card function

--- sqitch/deploy/card_functions.sql
+++ sqitch/deploy/card_functions.sql
@@ -3,8 +3,13 @@
 
 BEGIN;
 -- used to get the complete data of a card from the given json object
-CREATE FUNCTION magic_inventory.create_card(json) RETURNS magic_inventory.card AS $$
-
+CREATE FUNCTION magic_inventory.create_card(json) RETURNS magic_inventory.magic_card AS $$
+  SELECT tcg_id            FROM magic_inventory WHERE card.name = $1.name AND card.card_set = $1.card_set as temp_tcg_id;
+  SELECT image             FROM magic_inventory WHERE card.name = $1.name AND card.card_set = $1.card_set as temp_image;
+  SELECT set_code          FROM magic_inventory WHERE card.name = $1.name AND card.card_set = $1.card_set as temp_set_code;
+  SELECT set_name          FROM magic_inventory WHERE card.name = $1.name AND card.card_set = $1.card_set as temp_set_name;
+  SELECT collectors_number FROM magic_inventory WHERE card.name = $1.name AND card.card_set = $1.card_set as temp_collectors_number;
+  RETURN ROW($1.name, temp_tcg_id, temp_image, $1.card_set, temp_set_code, temp_set_name, temp_collectors_number, $1.condition $1.variations)::magic_inventory.card;
 $$ LANGUAGE SQL;
 
 COMMIT;
