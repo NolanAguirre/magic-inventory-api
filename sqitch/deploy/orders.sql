033855d118f632e7e22afbae1fc43b6584469667
Nolan Aguirre <nolanaguirre08@gmail.com>
2018-05-08 00:01:59 -0500

    add order status

--- sqitch/deploy/orders.sql
+++ sqitch/deploy/orders.sql
@@ -8,7 +8,8 @@ CREATE TABLE magic_inventory.orders(
     cards magic_inventory.magic_card[],
     store_id text,
     user_id text,
-    order_id SERIAL UNIQUE,
+    order_id SERIAL UNIQUE, -- needs to change probably
+    order_status magic_inventory.order_status,
     created_at timestamp default now()
 );
 
