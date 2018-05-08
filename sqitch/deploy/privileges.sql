540f1d9105c1cfe02d7bb88e94f676e8def9fbba
Nolan Aguirre <nolanaguirre08@gmail.com>
2018-05-08 00:02:51 -0500

    update privileges before testing

--- sqitch/deploy/privileges.sql
+++ sqitch/deploy/privileges.sql
@@ -14,11 +14,11 @@ GRANT SELECT ON TABLE magic_inventory.inventory TO magic_inventory_user, magic_i
 GRANT DELETE ON TABLE magic_inventory.inventory TO magic_inventory_employee, magic_inventory_store_owner; --insert and delete via function
 
 GRANT SELECT ON TABLE magic_inventory.orders TO magic_inventory_user, magic_inventory_employee, magic_inventory_store_owner;
-GRANT UPDATE ON TABLE magic_inventory.orders TO magic_inventory_employee, magic_inventory_store_owner; -- insert via function
+GRANT UPDATE ON TABLE magic_inventory.orders TO magic_inventory_employee, magic_inventory_store_owner; -- insert via function, trigger needed
 
-GRANT SELECT ON TABLE magic_inventory.stores TO magic_inventory_user, magic_inventory_anonymous, magic_inventory_employee, magic_inventory_store_owner;
-GRANT UPDATE (admins) ON TABLE magic_inventory.stores TO magic_inventory_store_owner;
-GRANT UPDATE (client_settings) ON TABLE magic_inventory.stores TO magic_inventory_employee, magic_inventory_store_owner;
+GRANT SELECT (store_name, store_id) ON TABLE magic_inventory.stores TO magic_inventory_user, magic_inventory_anonymous, magic_inventory_employee, magic_inventory_store_owner;
+GRANT SELECT (client_settings), UPDATE (client_settings) ON TABLE magic_inventory.stores TO magic_inventory_employee, magic_inventory_store_owner;
+GRANT SELECT (admins) , UPDATE (admins) ON TABLE magic_inventory.stores TO magic_inventory_store_owner
 
 -- this needs some thinkin done before i do this
 
