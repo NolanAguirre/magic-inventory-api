cee97bb99efbdddf70d9354ec6964c2eee4f4be4
Nolan Aguirre <nolanaguirre08@gmail.com>
2018-05-12 20:18:41 -0500

    fix the add_user function so it works

--- sqitch/deploy/user_functions.sql
+++ sqitch/deploy/user_functions.sql
@@ -7,24 +7,19 @@
 BEGIN;
 
 --TODO remove return query, change to remove
-CREATE FUNCTION magic_inventory.add_user(arg_user_name CITEXT,arg_user_id TEXT, arg_user_email CITEXT) RETURNS SETOF BOOLEAN AS $$
-
+CREATE FUNCTION magic_inventory.add_user(arg_user_name CITEXT,arg_user_id TEXT, arg_user_email CITEXT) RETURNS BOOLEAN AS $$
   BEGIN
-
     IF (SELECT EXISTS(SELECT 1 FROM magic_inventory.users WHERE (app_user).user_id = $2)) THEN
-      RETURN QUERY SELECT false;
+      RETURN false;
+    ELSE
+      INSERT INTO magic_inventory.users VALUES (ROW($1, $2, $3)::magic_inventory.user_type);
+      IF (SELECT EXISTS(SELECT 1 FROM magic_inventory.users WHERE (app_user).user_id = $2)) THEN -- this maybe uneeded
+        RETURN true;
+      ELSE
+        RETURN false;
+      END IF;
     END IF;
-
-    INSERT INTO magic_inventory.users VALUES (ROW($1, $2, $3)::magic_inventory.user_type);
-
-    IF (SELECT EXISTS(SELECT 1 FROM magic_inventory.users WHERE (app_user).user_id = $2)) THEN -- this maybe uneeded
-      RETURN QUERY SELECT false;
-    END IF;
-
-    RETURN QUERY SELECT true;
-
   END
-
 $$ LANGUAGE PLPGSQL;
 
 CREATE FUNCTION magic_inventory.remove_user(arg_user_id TEXT) RETURNS void AS $$
