935cdbe03fe5232780ed3c263668b755159f8b4d
Nolan Aguirre <nolanaguirre08@gmail.com>
2018-05-30 15:13:41 -0500

    reorder order_functions migration

--- sqitch/deploy/store_functions.sql
+++ sqitch/deploy/store_functions.sql
@@ -2,11 +2,6 @@
 -- requires: stores
 
 BEGIN;
-
--- CREATE FUNCTION magic_inventory.create_store(arg_store_name citext) RETURNS VOID AS $$
---
--- $$ LANGUAGE SQL;
---
 -- CREATE FUNCTION magic_inventory.update_store_settings(arg_settings json) RETURNS VOID AS $$
 --
 -- $$ LANGUAGE SQL;
