79a764e66be6be2ce81ec2ce3961e01f9e2454dc
Nolan Aguirre <nolanaguirre08@gmail.com>
2018-06-14 10:03:53 -0500

    remove card functions migration

--- sqitch/revert/card_functions.sql
+++
@@ -1,7 +0,0 @@
--- Revert magic-inventory:card_functions from pg
-
-BEGIN;
-
-DROP FUNCTION magic_inventory.typeahead(CITEXT);
-
-COMMIT;
--- sqitch/verify/card_functions.sql
+++
@@ -1,6 +0,0 @@
--- Verify magic-inventory:card_functions on pg
-
-BEGIN;
-
-
-ROLLBACK;
