f9b3ff52636105362d21349c8541f230ae8b2776
Nolan Aguirre <nolanaguirre08@gmail.com>
2018-07-16 11:09:19 -0500

    remove uneeded functions

--- sqitch/deploy/store_functions.sql
+++ sqitch/deploy/store_functions.sql
@@ -2,12 +2,6 @@
 -- requires: stores
 
 BEGIN;
--- CREATE FUNCTION magic_inventory.update_store_settings(arg_settings json) RETURNS VOID AS $$
---
--- $$ LANGUAGE SQL;
---
--- CREATE FUNCTION magic_inventory.delete_store(arg_store_id TEXT) RETURNS VOID as $$
---
--- $$ LANGUAGE SQL;
+
 
 COMMIT;
