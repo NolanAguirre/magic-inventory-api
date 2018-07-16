f2df2a6e80e4c7fa052d9dc98c723c17b739e92f
Nolan Aguirre <nolanaguirre08@gmail.com>
2018-07-16 10:56:25 -0500

    add privileges to many tables

--- sqitch/deploy/privileges.sql
+++ sqitch/deploy/privileges.sql
@@ -6,22 +6,29 @@
 BEGIN;
 
 ALTER DEFAULT PRIVILEGES REVOKE EXECUTE ON functions FROM public;
-
+-- schema
 GRANT USAGE ON SCHEMA magic_inventory TO magic_inventory_user, magic_inventory_anonymous, magic_inventory_employee, magic_inventory_store_owner;
-
+-- admins table
+GRANT INSERT, DELETE ON TABLE magic_inventory.admins TO magic_inventory_store_owner;
+-- cards table
 GRANT SELECT ON TABLE magic_inventory.cards TO magic_inventory_user, magic_inventory_anonymous, magic_inventory_employee, magic_inventory_store_owner;
-
-
+-- stores table
 GRANT SELECT (name, id) ON TABLE magic_inventory.stores TO magic_inventory_user, magic_inventory_anonymous, magic_inventory_employee, magic_inventory_store_owner;
 GRANT SELECT (client_settings), UPDATE (client_settings) ON TABLE magic_inventory.stores TO magic_inventory_employee, magic_inventory_store_owner;
-
--- GRANT EXECUTE ON FUNCTION magic_inventory.add_user(CITEXT, TEXT, CITEXT) TO magic_inventory_user, magic_inventory_anonymous, magic_inventory_employee, magic_inventory_store_owner;
--- GRANT EXECUTE ON FUNCTION magic_inventory.remove_user(TEXT) TO magic_inventory_user, magic_inventory_employee, magic_inventory_store_owner;
---
--- GRANT EXECUTE ON FUNCTION magic_inventory.add_inventory(json[], TEXT) TO magic_inventory_employee, magic_inventory_store_owner;
--- GRANT EXECUTE ON FUNCTION magic_inventory.remove_inventory(json[], TEXT) TO magic_inventory_employee, magic_inventory_store_owner;
-
-
+-- orders table
+GRANT INSERT, SELECT ON TABLE magic_inventory.orders TO magic_inventory_user, magic_inventory_employee, magic_inventory_store_owner;
+GRANT UPDATE (order_status) ON TABLE magic_inventory.orders TO magic_inventory_user, magic_inventory_employee, magic_inventory_store_owner;
+-- order items table
+GRANT INSERT, SELECT ON TABLE magic_inventory.order_items TO magic_inventory_user, magic_inventory_employee, magic_inventory_store_owner;
+-- inventory items table
+GRANT SELECT ON TABLE magic_inventory.inventory TO magic_inventory_user, magic_inventory_anonymous, magic_inventory_employee, magic_inventory_store_owner;
+GRANT INSERT ON TABLE magic_inventory.inventory TO magic_inventory_employee, magic_inventory_store_owner;
+GRANT UPDATE (status) ON TABLE magic_inventory.inventory TO magic_inventory_user, magic_inventory_employee, magic_inventory_store_owner;
+-- users table
+GRANT INSERT ON TABLE magic_inventory.users TO magic_inventory_anonymous;
+--GRANT UPDATE (email) ON TABLE magic_inventory.users TO magic_inventory_user, magic_inventory_employee, magic_inventory_store_owner;
+GRANT SELECT (first_name, last_name, id) ON TABLE magic_inventory.users TO magic_inventory_store_owner;
+GRANT DELETE ON TABLE magic_inventory.users TO magic_inventory_user, magic_inventory_employee, magic_inventory_store_owner;
 -- row level needed:
 -- inventory, stores can only add and remove from their stores
 -- orders, users, and stores can only add to their orders, via checking if they belong to the store, or the person is the person who placed the order
