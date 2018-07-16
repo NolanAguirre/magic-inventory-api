d3167ebc597210a4eb6dd569b0125b9d8dd41ea6
Nolan Aguirre <nolanaguirre08@gmail.com>
2018-07-16 10:50:51 -0500

    add typeahead and query by name to inventory

--- sqitch/deploy/inventory_functions.sql
+++ sqitch/deploy/inventory_functions.sql
@@ -1,40 +1,23 @@
 -- Deploy magic-inventory:inventory_functions to pg
 -- requires: inventory
+-- require: cards
 
 BEGIN;
 
-CREATE FUNCTION magic_inventory.add_inventory(arg_cards_data json[], arg_store_id UUID) RETURNS VOID AS $$ -- used for the client
-  DECLARE
-    json_card json;
+CREATE FUNCTION magic_inventory.inventory_typeahead(arg_one CITEXT , arg_two UUID) RETURNS SETOF CITEXT AS $$ -- used for the client
   BEGIN
-    FOR json_card IN SELECT * FROM json_array_elements($1)
-    LOOP
-        INSERT INTO magic_inventory.inventory (card_id, store_id, condition) VALUES (json_card.cardId, arg_store_id, json_card.cardCondition);
-    END LOOP;
+  RETURN QUERY SELECT DISTINCT name FROM magic_inventory.cards WHERE id in (SELECT card_id FROM magic_inventory.inventory WHERE store_id = $2) AND name LIKE $1 LIMIT 10;
   END;
-$$ LANGUAGE PLPGSQL;
+$$ LANGUAGE PLPGSQL STABLE;
 
--- CREATE FUNCTION magic_inventory.add_inventory(arg_order_id UUID) RETURNS VOID AS $$ -- used for orders being canced
---   INSERT INTO magic_inventory.inventory (card_id, store_id, condition) SELECT card_id, store_id, condition FROM magic_inventory.orders WHERE arg_order_id = $1;
--- $$ LANGUAGE SQL;
-
-CREATE FUNCTION magic_inventory.remove_inventory(arg_card_ids UUID[]) RETURNS VOID AS $$ -- used for the client to manually remove inventory
-  DECLARE
-    card_id UUID;
+CREATE FUNCTION magic_inventory.inventory_by_card_name_and_store_id(arg_one CITEXT, arg_two UUID) RETURNS SETOF magic_inventory.inventory AS $$ -- used for the client
   BEGIN
-    FOREACH card_id IN ARRAY $1
-    LOOP
-        DELETE FROM magic_inventory.inventory WHERE id = $1;
-    END LOOP;
+  RETURN QUERY SELECT * FROM magic_inventory.inventory WHERE card_id in (SELECT id FROM magic_inventory.cards WHERE name = $1) AND store_id = $2;
   END;
-$$ LANGUAGE PLPGSQL;
+$$ LANGUAGE PLPGSQL STABLE;
 
--- CREATE FUNCTION magic_inventory.remove_inventory(arg_card_id UUID) RETURNS VOID AS $$ -- used for orders
---   DELETE FROM magic_inventory.inventory WHERE id = $1;
--- $$ LANGUAGE SQL;
--- COMMENT ON FUNCTION magic_inventory.add_inventory(json[], TEXT) is 'Accepts json array of cards and adds them to the stores inventory';
--- COMMENT ON FUNCTION magic_inventory.add_inventory(magic_inventory.inventory_card_type[], TEXT) is 'Internal use only.';
--- COMMENT ON FUNCTION magic_inventory.add_inventory(magic_inventory.inventory_card_type, TEXT) is 'Internal use only.';
--- COMMENT ON FUNCTION magic_inventory.remove_inventory(json[], TEXT) is 'Accepts json array of cards and removes them to the stores inventory';
+COMMENT ON FUNCTION magic_inventory.inventory_typeahead(CITEXT, UUID) is 'Typeahead for a stores inventory';
+COMMENT ON FUNCTION magic_inventory.inventory_by_card_name_and_store_id(CITEXT, UUID) is 'Custom function for querying a stores inventory for a card';
 
+-- TODO make this work like postgraphile queries, that accept json and work magically like
 COMMIT;
--- sqitch/revert/inventory_functions.sql
+++ sqitch/revert/inventory_functions.sql
@@ -2,13 +2,7 @@
 
 BEGIN;
 
-DROP FUNCTION magic_inventory.add_inventory(json[], UUID);
+DROP FUNCTION magic_inventory.inventory_typeahead(arg_one CITEXT , arg_two UUID);
 
---DROP FUNCTION magic_inventory.add_inventory(UUID);
-
-DROP FUNCTION magic_inventory.remove_inventory(UUID[]);
-
---DROP FUNCTION magic_inventory.remove_inventory(UUID);
-
-
+DROP FUNCTION magic_inventory.inventory_by_card_name_and_store_id(arg_one CITEXT, arg_two UUID);
 COMMIT;
