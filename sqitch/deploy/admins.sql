7239b182cc51387424c8bd388ab81353ca448dd7
Nolan Aguirre <nolanaguirre08@gmail.com>
2018-08-17 00:11:42 -0500

    add cascade statements to admins

--- sqitch/deploy/admins.sql
+++ sqitch/deploy/admins.sql
@@ -6,8 +6,8 @@ BEGIN;
 
 CREATE TABLE magic_inventory.admins(
   id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-  user_id UUID REFERENCES magic_inventory.users(id),
-  store_id UUID REFERENCES magic_inventory.stores(id),
+  user_id UUID REFERENCES magic_inventory.users(id) ON DELETE CASCADE,
+  store_id UUID REFERENCES magic_inventory.stores(id) ON DELETE CASCADE,
   CONSTRAINT admins_key UNIQUE (user_id, store_id)
 );
 
