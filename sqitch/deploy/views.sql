239a7cdca7fa81a6f6315ed5747307bccecc19ee
Nolan Aguirre <nolanaguirre08@gmail.com>
2018-08-17 00:13:49 -0500

    add views to take the place of inventory functions

--- sqitch/deploy/views.sql
+++ sqitch/deploy/views.sql
@@ -5,8 +5,21 @@ BEGIN;
 
 CREATE VIEW magic_inventory.card_name AS SELECT DISTINCT name FROM magic_inventory.cards;
 
-CREATE VIEW magic_inventory.card_set AS SELECT DISTINCT set_name from magic_inventory.cards;
+CREATE VIEW magic_inventory.card_set AS SELECT DISTINCT set_name FROM magic_inventory.cards;
 
+CREATE VIEW magic_inventory.inventory_card_set AS SELECT
+DISTINCT magic_inventory.cards.set_name,
+magic_inventory.inventory.store_id
+FROM magic_inventory.inventory
+INNER JOIN magic_inventory.cards ON (magic_inventory.inventory.card_id = magic_inventory.cards.id);
+
+CREATE VIEW magic_inventory.inventory_card_name AS SELECT
+DISTINCT magic_inventory.cards.name,
+magic_inventory.inventory.store_id
+FROM magic_inventory.inventory
+INNER JOIN magic_inventory.cards ON (magic_inventory.inventory.card_id = magic_inventory.cards.id);
+
+-- CREATE VIEW magic_inventory.inventory_card
 COMMIT;
 
 -- create view magic_inventory.test_view as select
